832dc4d21a2138b24cdf16b614f1029f2cc57b4d
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-02-14 00:25:41 +0530

    create vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: true,
+    chunkSizeWarningLimit: 1600,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
+  }
 })
